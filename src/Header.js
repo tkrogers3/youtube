f4aaed66d0bf203c7a5c193c83a7db718b482ada
tkrogers3 <tkrogers3@gmail.com>
2020-11-12 16:46:26 -0500

    initiailzed youtube clone. Header bar.

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallOutlinedIcon from '@material-ui/icons/VideoCallOutlined';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsActiveOutlinedIcon from '@material-ui/icons/NotificationsActiveOutlined';
+import Avatar from '@material-ui/core/Avatar';
+
+
+function Header() {
+    return (
+        <div className="header">
+
+<MenuIcon />
+<img 
+className="header_logo"
+src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
+alt="YouTube logo">
+</img>
+<input type="text"/>
+<SearchIcon />
+<VideoCallOutlinedIcon/>
+<AppsIcon />
+<NotificationsActiveOutlinedIcon/>
+<Avatar 
+    img src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/119007074_10157520994512541_7477164819350937676_n.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=kLj08tp2EA0AX8HiDWs&_nc_ht=scontent-lga3-1.xx&oh=d19abb82ef3da4764584a19d146a8acc&oe=5FD1D37C"
+    alt="user image"
+/>
+
+       </div>
+    )
+}
+
+export default Header
